cb36a6d97af78fedda02ef06e97effdda1a44dcc
hmh <396169330@qq.com>
2018-11-29 15:46:00 +0800

    启动文件

--- manager.py
+++ manager.py
@@ -1,1 +1,36 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import  CSRFProtect
+from redis import StrictRedis
+from flask_session import Session
+from datetime import timedelta
+
+
+app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "qawsedrf"
+
+app.config["SQLALCHEMY_DATABASE_URI"]="mysql+pymysql://root:mysql@localhost:3306/information_hmh"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=False
+
+db = SQLAlchemy(app)
+
+redis_info = StrictRedis(host="localhost",port=6379)
+
+app.config["SESSION_TYPE"] = "redis"
+app.config["SESSION_USE_SIGNER"] = True
+app.config["SESSION_REDIS"] = StrictRedis(host="localhost",port=6379)
+app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=2)
+
+
+CSRFProtect(app)
+
+Session(app)
+
+@app.route('/')
+def index():
+
+    return ('hello word!')
+
+if __name__ == '__main__':
+    app.run()
