2813dc6b977602b78b2cf28b77bb24baffbc9bcd
hmh <396169330@qq.com>
2018-12-03 15:49:01 +0800

    完善新闻详细信息

--- info/modules/news/views.py
+++ info/modules/news/views.py
@@ -1,4 +1,4 @@
-from flask import session, jsonify,render_template
+from flask import session, jsonify,render_template,current_app
 from info import models
 from info.utils.response_code import RET
 from . import news_blue
@@ -13,6 +13,7 @@ def news_item(num):
             # user = models.User.query.filter(models.User.id == session_id).first()
             user = models.User.query.get(session_id)
         except Exception as e:
+            current_app.logger.error(e)
             return jsonify(errno=RET.DBERR,errmsg="数据库查询失败!")
 
     # 使用num查询新闻信息
@@ -22,6 +23,7 @@ def news_item(num):
         # news = models.News.query.filter(models.News.id == num).first()
         news = models.News.query.get(num)
     except Exception as e:
+        current_app.logger.error(e)
         return jsonify(errno=RET.DBERR,errmsg="数据库查询失败!")
 
     # news_list= [ ]
@@ -29,9 +31,22 @@ def news_item(num):
     # 放入列表中是为了在模板中方便遍历,并不是所有数据都需要放入列表中！！！
     # print(news_list)
     # print (user)
+
+    #热门数据
+    try:
+        news_list = models.News.query.order_by(models.News.create_time.desc()).limit(6).all()
+    except Exception as e:
+        current_app.logger.error(e)
+        return jsonify(errno=RET.DBERR,errmsg="数据库查询失败!")
+
+    n_news_list = [ ]
+    for item in news_list:
+        n_news_list.append(item.to_dict())
+
     data = {
         "user_info":user.to_dict() if user else "" ,#else 后面添加的需要是None 并不能是 “ ”这样表明是一个空格
-        "news_info":news.to_dict()
+        "news_info":news.to_dict(),
+        "n_news_list" :n_news_list
     }
 
     return render_template("news/detail.html",data=data)
